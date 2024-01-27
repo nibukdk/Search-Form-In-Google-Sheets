e042fc4a6a1abd4ea7578f8f87804e0b80e3f5b4
nibukdk <nibeshkhadka@gmail.com>
2024-01-27 15:42:41 +0200

    Search Form in Google Sheets Codes and Assets

---
+++ search.js
@@ -0,0 +1,149 @@
+var ss = SpreadsheetApp.getActiveSpreadsheet();
+var searchSheet = ss.getSheetByName("search");
+var _2023Sheet = ss.getSheetByName("2023");
+var _2022Sheet = ss.getSheetByName("2022");
+var _2021Sheet = ss.getSheetByName("2021");
+
+var nameRangeNotation = 'A2:A'
+var descriptionRangeNotation = 'F2:F'
+var quantityRangeNotation = 'E2:E'
+
+function search() {
+  try {
+    SpreadsheetApp.getActiveSpreadsheet().toast("Searching Throught Your Database...", 'Searching');
+
+    const clientName = searchSheet.getRange('B2:C2').getValue();
+    const quantity = searchSheet.getRange('E2').getValue();
+    const description = searchSheet.getRange('G2:H2').getValue();
+    const hasIncludeAllSelected = searchSheet.getRange('G4:G5').getValue();
+
+
+    // console.log(clientName)
+    // console.log(quantity)
+    // console.log(description)
+    // console.log(hasIncludeAllSelected)
+
+    let status;
+
+    if (hasIncludeAllSelected) {
+      //perform AND search
+      const newData = andSearch(clientName, description, quantity);
+
+
+
+      status = fillSearchWithResults(searchSheet.getDataRange().getValues(), newData)
+      // console.log(status);
+      if (status === 400) { throw new Error(SEARCH_STATUS.SEARCH_FAILURE); }
+    }
+    else {
+      //perform OR serach
+      let newData = orSearch(clientName, description, quantity);
+
+      status = fillSearchWithResults(searchSheet.getDataRange().getValues(), newData)
+      // console.log(status);
+
+      if (status === 400) { throw new Error(SEARCH_STATUS.SEARCH_FAILURE); }
+    }
+
+    if (status === 200) {
+      SpreadsheetApp.getActiveSpreadsheet().toast(SEARCH_STATUS.SEARCH_SUCCESFULL, 'Success');
+    }
+
+  } catch (e) {
+    // console.log(e)
+    if (e.Error === SEARCH_STATUS.AND_SEARCH_FAILURE || e.Error === SEARCH_STATUS.SEARCH_FAILURE) {
+      SpreadsheetApp.getActiveSpreadsheet().toast(SEARCH_STATUS.SEARCH_FAILURE, 'Not Found!');
+    
+    }else{
+      SpreadsheetApp.getActiveSpreadsheet().toast(`<font color="red"> ${e} </font>`, 'Error!');
+
+    }
+
+  }
+}
+
+
+function andSearch(name = null, description = null, quantity = null) {
+  // get matching index for each sheet.
+  const _2021SheetNameSearchIndexes = searchSheetByColumn(_2021Sheet, nameRangeNotation, name);
+  const _2021SheetQuantitySearchIndexes = searchSheetByColumn(_2021Sheet, quantityRangeNotation, quantity);
+  const _2021SheetDescriptionSearchIndexes = searchSheetByColumn(_2021Sheet, descriptionRangeNotation, description);
+
+
+  const _2022SheetNameSearchIndexes = searchSheetByColumn(_2022Sheet, nameRangeNotation, name);
+  const _2022SheetQuantitySearchIndexes = searchSheetByColumn(_2022Sheet, quantityRangeNotation, quantity);
+  const _2022SheetDescriptionSearchIndexes = searchSheetByColumn(_2022Sheet, descriptionRangeNotation, description);
+
+  const _2023SheetNameSearchIndexes = searchSheetByColumn(_2023Sheet, nameRangeNotation, name);
+  const _2023SheetQuantitySearchIndexes = searchSheetByColumn(_2023Sheet, quantityRangeNotation, quantity);
+  const _2023SheetDescriptionSearchIndexes = searchSheetByColumn(_2023Sheet, descriptionRangeNotation, description);
+
+
+  // matching indexes of rows in AND search
+  const _2021SheetMatchingRowsIndexes = filterRowsIndexesWithAllSearchTerms(_2021SheetNameSearchIndexes, _2021SheetQuantitySearchIndexes, _2021SheetDescriptionSearchIndexes);
+  const _2022SheetMatchingRowsIndexes = filterRowsIndexesWithAllSearchTerms(_2022SheetNameSearchIndexes, _2022SheetQuantitySearchIndexes, _2022SheetDescriptionSearchIndexes);
+  const _2023SheetMatchingRowsIndexes = filterRowsIndexesWithAllSearchTerms(_2023SheetNameSearchIndexes, _2023SheetQuantitySearchIndexes, _2023SheetDescriptionSearchIndexes);
+
+  // get data from row indexes
+  const _2021SheetMatchingRows = fetchDataByRowIndexes(_2021Sheet, _2021SheetMatchingRowsIndexes)
+  const _2022SheetMatchingRows = fetchDataByRowIndexes(_2022Sheet, _2022SheetMatchingRowsIndexes)
+  const _2023SheetMatchingRows = fetchDataByRowIndexes(_2023Sheet, _2023SheetMatchingRowsIndexes)
+
+  // filter duplicate rows
+  const _2021SheetMatchingUniqueRows = filterDuplicateRows(_2021SheetMatchingRows);
+  const _2022SheetMatchingUniqueRows = filterDuplicateRows(_2022SheetMatchingRows);
+  const _2023SheetMatchingUniqueRows = filterDuplicateRows(_2023SheetMatchingRows);
+
+
+  const andSearchResult = [..._2023SheetMatchingUniqueRows, ..._2022SheetMatchingUniqueRows, ..._2021SheetMatchingUniqueRows]
+
+  if (andSearchResult.length < 0) return;
+
+  return andSearchResult;
+
+}
+
+
+function orSearch(name = null, description = null, quantity = null) {
+  // get matching index for each sheet.
+  const _2021SheetNameSearchIndexes = searchSheetByColumn(_2021Sheet, nameRangeNotation, name);
+  const _2021SheetQuantitySearchIndexes = searchSheetByColumn(_2021Sheet, quantityRangeNotation, quantity);
+  const _2021SheetDescriptionSearchIndexes = searchSheetByColumn(_2021Sheet, descriptionRangeNotation, description);
+
+
+  const _2022SheetNameSearchIndexes = searchSheetByColumn(_2022Sheet, nameRangeNotation, name);
+  const _2022SheetQuantitySearchIndexes = searchSheetByColumn(_2022Sheet, quantityRangeNotation, quantity);
+  const _2022SheetDescriptionSearchIndexes = searchSheetByColumn(_2022Sheet, descriptionRangeNotation, description);
+
+  const _2023SheetNameSearchIndexes = searchSheetByColumn(_2023Sheet, nameRangeNotation, name);
+  const _2023SheetQuantitySearchIndexes = searchSheetByColumn(_2023Sheet, quantityRangeNotation, quantity);
+  const _2023SheetDescriptionSearchIndexes = searchSheetByColumn(_2023Sheet, descriptionRangeNotation, description);
+
+  // get values from those indexes
+  const _2021SheetNameSearch = fetchDataByRowIndexes(_2021Sheet, _2021SheetNameSearchIndexes);
+  const _2021SheetQuantitySearch = fetchDataByRowIndexes(_2021Sheet, _2021SheetQuantitySearchIndexes);
+  const _2021SheetDescriptionSearch = fetchDataByRowIndexes(_2021Sheet, _2021SheetDescriptionSearchIndexes);
+
+  const _2022SheetNameSearch = fetchDataByRowIndexes(_2022Sheet, _2022SheetNameSearchIndexes);
+  const _2022SheetQuantitySearch = fetchDataByRowIndexes(_2022Sheet, _2022SheetQuantitySearchIndexes);
+  const _2022SheetDescriptionSearch = fetchDataByRowIndexes(_2022Sheet, _2022SheetDescriptionSearchIndexes);
+
+  const _2023SheetNameSearch = fetchDataByRowIndexes(_2023Sheet, _2023SheetNameSearchIndexes);
+  const _2023SheetQuantitySearch = fetchDataByRowIndexes(_2023Sheet, _2023SheetQuantitySearchIndexes);
+  const _2023SheetDescriptionSearch = fetchDataByRowIndexes(_2023Sheet, _2023SheetDescriptionSearchIndexes);
+
+
+
+  // filter duplicate rows
+  const _2021SheetMatchingUniqueRows = filterDuplicateRows([..._2021SheetNameSearch, ..._2021SheetQuantitySearch, ..._2021SheetDescriptionSearch]);
+  const _2022SheetMatchingUniqueRows = filterDuplicateRows([..._2022SheetNameSearch, ..._2022SheetQuantitySearch, ..._2022SheetDescriptionSearch]);
+  const _2023SheetMatchingUniqueRows = filterDuplicateRows([..._2023SheetNameSearch, ..._2023SheetQuantitySearch, ..._2023SheetDescriptionSearch]);
+
+  const orSearchResult = [..._2021SheetMatchingUniqueRows, ..._2022SheetMatchingUniqueRows, ..._2023SheetMatchingUniqueRows]
+
+  if (orSearchResult.length < 0) return;
+
+  return orSearchResult;
+
+}
+
---
+++ utils.js
@@ -0,0 +1,127 @@
+
+const SEARCH_STATUS = {
+    SEARCH_SUCCESFULL: "Search Was Successfull!",
+    // OR_SEARCH_SUCCESFULL: "Search Was Successfull!",
+    SEARCH_FAILURE: "No items we found in the given given critera!",
+    //OR_SEARCH_FAILURE: "No items we found in the given given critera!"
+}
+
+/**
+ * To Fill search sheet with values
+ * @param {Array<Array<Srting>>}  oldData - previous search results data
+ * @param {Array<Array<Srting>>}  newData - new search result to fill
+ */
+function fillSearchWithResults(oldData, newData) {
+    // console.log("Inside fillSearchWithResults() old data:", oldData.length);
+    if (oldData.length > 0) {
+        searchSheet.getRange(8, 1, oldData.length-7, 9).clear(); // clear until last filled data
+    }
+    SpreadsheetApp.flush();
+    Utilities.sleep(1000);
+    console.log("Inside fillSearchWithResults() new Data:", newData);
+    if (newData.length < 1) return 400;
+    searchSheet.getRange(8, 1, newData.length, 9).setValues(newData);
+    return 200;
+}
+
+
+/**
+ * Searches the given keyword in the given column inside the given spreadsheet tab.
+ * It returns all the matching indexes of data. Indexes are index from array not row.
+ * @param {Spreadsheet} sheet - sheet to search from
+ * @param {String} rangeNotation - range of the column in the given spreadsheet
+ * @param {String} searchVal - keyword to search
+ * @returns {Array<number>} - [1,23,12,45,12] 
+ */
+function searchSheetByColumn(sheet = _2023Sheet, rangeNotation = descriptionRangeNotation, searchVal = "courier") {
+    const data = sheet.getRange(rangeNotation).getValues().flat().filter(String); // get data
+
+    if (data.length < 1) return [];
+    // filter only matching rows indexes
+    // got from https://stackoverflow.com/a/58980987/6163929
+    const allIndexes = data.map((val, index) => ({ val, index }))
+        .filter(({ val, index }) => rangeNotation === quantityRangeNotation ? val == searchVal : val.toLowerCase().includes(searchVal.toLowerCase()))
+        .map(({ _, index }) => index + 1) // +1 because we extract data from second row in notation later on have to match with whole data array
+
+    // console.log("Inside searchSheetByColumn()");
+
+    // console.log(data)
+    // console.log(data.length)
+    // console.log(allIndexes)
+    // console.log(allIndexes.length)
+    // console.log("Outside searchSheetByColumn()");
+
+    return allIndexes;
+}
+
+/**
+ * Funciton extracts the rows of provided indexes+1, from the given spreadsheet tab.
+ * @param {Spreadsheet} sheet - sheet to search from
+ * @param {Array<number>} indexes - indexes of row to extract values.
+ * @returns {Array<Array<Srting>>} - Arrays of nested rows in the indexes from the given sheet.
+ */
+function fetchDataByRowIndexes(sheet = _2021Sheet, indexes = []) {
+    // console.log("Inside fetchDataByRowIndexes() provided indexes are:" + indexes)
+
+    if (indexes.length < 1) return [];
+
+    const data = sheet.getDataRange().getValues();
+    const newData = [];
+
+    for (let i = 0; i < indexes.length; i++) {
+        // push [row, rowIndex] 
+        newData.push([...data[indexes[i]], `${sheet.getName()} - ${indexes[i] + 1}`])
+    }
+    // console.log("Inside fetchDataByRowIndexes() data from procvided indexes:" + newData)
+
+    console.log(newData);
+
+    return newData;
+}
+
+/**
+ * Function filters only rows that consist all three keywords provided by user input
+ * @param {Array<String>} arr1 - 
+ * @param {Array<String>} arr2 
+ * @param {Array<String>} arr3 
+ * @returns {Array<String>?} 
+ */
+function filterRowsIndexesWithAllSearchTerms(arr1, arr2, arr3) {
+    /*   console.log("Inside filterRowsIndexesWithAllSearchTerms() arr1:" + arr1)
+      console.log("Inside filterRowsIndexesWithAllSearchTerms() arr2:" + arr2)
+      console.log("Inside filterRowsIndexesWithAllSearchTerms() arr3:" + arr3)
+   */
+    const matchingIndexes = [];
+    arr1.forEach((val) => {
+        if (arr2.includes(val) && arr3.includes(val)) {
+            matchingIndexes.push(val)
+        }
+
+    });
+    // console.log("Inside filterRowsIndexesWithAllSearchTerms() mathcingIndexes:" + matchingIndexes)
+
+    return matchingIndexes;
+
+}
+
+
+/**
+ * Takes Duplicate data that might have resulted from different individual column searches and only returns unique rows 
+ * in each column from the serach results.
+ * @param {Array<String>} arr 
+ * @returns {Array<String>}- [[],[]]
+ */
+function filterDuplicateRows(arr) {
+    if (arr.length < 1) return [];
+    const delimiter = "*---*--*";
+    // console.log("Inside filterDuplicateRows() arr to check:" + arr)
+
+    const strArr = arr.map(row => row.join(delimiter)).flat();
+    // console.log("Inside filterDuplicateRows() strArr:" + strArr)
+
+    const uniqueArrays = [...new Set(strArr)].map(str => str.split(delimiter))
+    // console.log("Inside filterDuplicateRows() uniqueArrays:" + uniqueArrays)
+
+    return uniqueArrays;
+
+}
